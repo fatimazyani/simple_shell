1d8a02e0777f23b5460b3033d4395487e84c52d3
fatimazyani <123373140+fatimazyani@users.noreply.github.com>
2023-05-26 01:20:42 +0200

    Update memory.c

--- memory.c
+++ memory.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * bfree - frees a pointer and NULLs the address
- * @ptr: address of the pointer to free
+ * bfree - will free a given pointer
+ * @ptr: the address of the given pointer to free
  *
- * Return: 1 if freed, otherwise 0.
+ * Return: if freed return 1, otherwise 0.
  */
 int bfree(void **ptr)
 {
