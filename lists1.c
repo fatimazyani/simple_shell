3f75231ea8dc4f75be8206db2bcff242339c2478
fatimazyani <123373140+fatimazyani@users.noreply.github.com>
2023-05-26 01:18:35 +0200

    Update lists1.c

--- lists1.c
+++ lists1.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * list_len - determines length of linked list
- * @h: pointer to first node
+ * list_len - will give the lenght of a list
+ * @h: the given pointer of the first node
  *
  * Return: size of list
  */
@@ -19,10 +19,10 @@ size_t list_len(const list_t *h)
 }
 
 /**
- * list_to_strings - returns an array of strings of the list->str
- * @head: pointer to first node
+ * list_to_strings - will return an array of strings of the list
+ * @head: the given pointer to the first node
  *
- * Return: array of strings
+ * Return: returns the array of strings
  */
 char **list_to_strings(list_t *head)
 {
@@ -56,8 +56,8 @@ char **list_to_strings(list_t *head)
 
 
 /**
- * print_list - prints all elements of a list_t linked list
- * @h: pointer to first node
+ * print_list - will prints all the elements of a list
+ * @h: the given pointer to the first node
  *
  * Return: size of list
  */
@@ -79,10 +79,10 @@ size_t print_list(const list_t *h)
 }
 
 /**
- * node_starts_with - returns node whose string starts with prefix
- * @node: pointer to list head
- * @prefix: string to match
- * @c: the next character after prefix to match
+ * node_starts_with - will return every node if its string start with prefix
+ * @node: the given pointer to the list head
+ * @prefix: the string to match
+ * @c: the next character after prefix
  *
  * Return: match node or null
  */
@@ -101,9 +101,9 @@ list_t *node_starts_with(list_t *node, char *prefix, char c)
 }
 
 /**
- * get_node_index - gets the index of a node
- * @head: pointer to list head
- * @node: pointer to the node
+ * get_node_index -will  get the index of a given node
+ * @head: the given pointer to the list head
+ * @node: the given pointer to the node
  *
  * Return: index of node or -1
  */
