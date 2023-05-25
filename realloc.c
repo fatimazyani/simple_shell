f5e5a197cb1fb3492f350ee73fc22b6ce1e387cd
fatimazyani <123373140+fatimazyani@users.noreply.github.com>
2023-05-26 01:29:14 +0200

    Update realloc.c

--- realloc.c
+++ realloc.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- **_memset - fills memory with a constant byte
- *@s: the pointer to the memory area
- *@b: the byte to fill *s with
- *@n: the amount of bytes to be filled
+ **_memset - will fill a given memory with a constant byte
+ *@s: the given pointer
+ *@b: the given byte to fill *s with
+ *@n: the number of bytes to  fill
  *Return: (s) a pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
@@ -17,8 +17,8 @@ char *_memset(char *s, char b, unsigned int n)
 }
 
 /**
- * ffree - frees a string of strings
- * @pp: string of strings
+ * ffree - will free a string of strings
+ * @pp: the given string of strings
  */
 void ffree(char **pp)
 {
@@ -32,12 +32,12 @@ void ffree(char **pp)
 }
 
 /**
- * _realloc - reallocates a block of memory
- * @ptr: pointer to previous malloc'ated block
- * @old_size: byte size of previous block
- * @new_size: byte size of new block
+ * _realloc - will move a block of memory
+ * @ptr: the given pointer to the previous block of memory
+ * @old_size: the size of the previous block of memory
+ * @new_size: the size of the new block of memory
  *
- * Return: pointer to da ol'block nameen.
+ * Return: return pointer of the new location.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
